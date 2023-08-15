44e83d07fc5babf30486d731ece3e8e41e2272a6
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2023-08-15 11:06:02 -0700

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://qoabainc:ZvjX4J2edjU8woYW@cluster0.u2orl1m.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<username>:<password>@cluster0.u2orl1m.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.test
 
