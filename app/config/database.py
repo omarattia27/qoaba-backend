6d8829ade230b1a6b0d894a154a4eaa54e545e60
alexwholland <alexwholland@gmail.com>
2023-08-15 14:03:50 -0700

    small fix

--- app/config/database.py
+++ app/config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://qoabainc:XuCh1MtdFcsYo2Sl@cluster0.u2orl1m.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<username>:<password>@cluster0.u2orl1m.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.test
 
