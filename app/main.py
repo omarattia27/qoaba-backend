4c54368c0c24af40b0e317ac52cfe44b0130e7b6
alexwholland <alexwholland@gmail.com>
2023-08-17 11:04:37 -0700

    Implemented CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.questions_route import question_api_router
 
 app = FastAPI()
+
+# Configure CORS to allow requests from your frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Add your frontend URL here
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(question_api_router)
