af48734f815b24bc956ed299695ab9718ebb34d5
bhavanvir <bhavanvir.r@gmail.com>
2023-09-08 21:16:25 -0700

    Now using the retro configuration for Gravatar instead of identicon

--- app/utils/gravatar.py
+++ app/utils/gravatar.py
@@ -4,7 +4,7 @@ import hashlib
 def generate_random_avatar_url(email: str) -> str:
     email_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
     avatar_service_base_url = "https://www.gravatar.com/avatar/"
-    default_avatar_option = "d=identicon"
+    default_avatar_option = "d=retro"
     avatar_url = f"{avatar_service_base_url}{email_hash}?{default_avatar_option}"
 
     return avatar_url
